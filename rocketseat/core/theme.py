f19d9e9de3bb21c6068b134ac640552cf68e9731
Lee Olayvar <lee@leenux-main.(none)>
2009-05-01 06:30:40 -0700

    Added override grouping code for all overrides in init. This will eventually be moved into separate functions i'm sure.

--- rocketseat/core/theme.py
+++ rocketseat/core/theme.py
@@ -36,8 +36,8 @@ def find_theme_rel_path(theme_script_name):
 class CSSFile(object):
     '''An object L{the theme class <Theme>} can interact with more easily.
     '''
-    
-    
+
+
     def __init__(self, file_location, ie_only=False,
                  media='screen, projection'):
         '''
@@ -45,7 +45,7 @@ class CSSFile(object):
         self.file_location = file_location
         self.ie_only = ie_only
         self.media = media
-    
+
     def render_html(self, base_theme_static_uri=None):
         '''
         @param base_theme_static_uri: If supplied, the uri for the css file
@@ -56,8 +56,8 @@ class CSSFile(object):
                 return '''<link rel="stylesheet"
                 href="%s/%s" type="text/css"
                 media="%s" />''' % (base_theme_static_uri,
-                                                self.file_location,
-                                                self.media)
+                                    self.file_location,
+                                    self.media)
             else:
                 return '''<!--[if IE]><link rel="stylesheet"
                 href="%s/%s" type="text/css"
@@ -96,24 +96,24 @@ class Theme(object):
             self.theme_static_uri = '/static/themes/core/%s' % theme_script_name
         else:
             raise core.error.ThemeNotFoundError(theme_script_name)
-        
+
         # Import the user theme module, and grab the settings from it.
         theme_module = __import__('%s.theme' % self.theme_py_path,
                                   fromlist=['a'])
-        
+
         #: A list of L{css file <CSSFile>} like objects.
         self.css_files = list(theme_module.css_files)
         #: A list of L{js file <JSFile>} like objects.
         self.js_files = list(theme_module.js_files)
-        
+
         self._rendered_css_html = None
         self._rendered_js_html = None
         self._rendered_blocks_html = ''
-        
+
         ## Trigger the event, so other plugins can add to the css.
         #plugin_css = event_controller.call_listeners(
             #'core_p', 'add_plugin_css')
-        
+
         # To make sure atleast 1 handler works (and thus, avoiding a key error)
         # we append the generic, base, block, region, and page handlers to 
         # theme module lists.
@@ -133,7 +133,7 @@ class Theme(object):
                 '    ${base_body}'
                 '</body>'
                 '</html>'
-                ))
+            ))
         )
         theme_module.blocks.append(
             BlockTemplateOverride(template_text=(
@@ -143,7 +143,7 @@ class Theme(object):
                 '        ${block_content}'
                 '    </div>'
                 '</div>'
-                ))
+            ))
         )
         theme_module.pages.append(
             PageTemplateOverride(template_text=(
@@ -161,7 +161,7 @@ class Theme(object):
                 '</div>'
             ))
         )
-        
+
         # For the sake of speed on cached loads, we will sort the overrides
         # on a few criteria now, so that later consecutive cached page loads
         # can benefit from this sorting. What we do is group types of overrides
@@ -183,16 +183,30 @@ class Theme(object):
         # - An override with no name or uri (Only the first instance of this
         #   will match, seeing as the first occurance will _always_ match, the
         #   following ones can be ignore.)
-        
+
+        # The keys are the uri regex string.
+        base_overrides_by_uri = {}
+        # The first occurance of a "match all" override. All others ignored.
+        base_override_matching_all = None
+        for base in theme_module.bases:
+            if base.uri_match is not None:
+                base_overrides_by_uri[base.uri_match] = base
+            elif base_override_matching_all is None:
+                base_override_matching_all = base
+
+        self.base_overrides_by_uri = base_overrides_by_uri
+        self.base_override_matching_all = base_override_matching_all
+
         # The keys are the block names
         block_overrides_by_name = {}
-        # The keys are the blocks uri matches
+        # The keys are the block override's uri regex string
         block_overrides_by_uri = {}
-        # The keys are the names, and then uri's are looked up on eatch
+        # The keys are the names, and then uri's are looked up on each name
+        # match
         block_overrides_by_uri_and_name = {}
         # The first occurance of a "match all" override. All others are ignored.
         block_override_matching_all = None
-        
+
         for block in theme_module.blocks:
             if block.uri_match is not None:
                 if block.name is None:
@@ -211,56 +225,92 @@ class Theme(object):
         self.block_overrides_by_uri = block_overrides_by_uri
         self.block_overrides_by_uri_and_name = block_overrides_by_uri_and_name
         self.block_override_matching_all = block_override_matching_all
-        
-        self.page_handlers = theme_module.pages
-        self.region_handlers = theme_module.regions
-        
-    
+
+        # The keys are the uri regex string.
+        page_overrides_by_uri = {}
+        # The first occurance of a "match all" override. All others ignored.
+        page_override_matching_all = None
+        for page in theme_module.pages:
+            if page.uri_match is not None:
+                page_overrides_by_uri[page.uri_match] = page
+            elif page_override_matching_all is None:
+                page_override_matching_all = page
+
+        self.page_overrides_by_uri = page_overrides_by_uri
+        self.page_override_matching_all = page_override_matching_all
+
+        # The keys are the region names
+        region_overrides_by_name = {}
+        # The keys are the region override's uri regex string
+        region_overrides_by_uri = {}
+        # The keys are the names, and then uri's are looked up on each name
+        # match
+        region_overrides_by_uri_and_name = {}
+        # The first occurance of a "match all" override. All others are ignored.
+        region_override_matching_all = None
+
+        for region in theme_module.regions:
+            if region.uri_match is not None:
+                if region.name is None:
+                    region_overrides_by_uri[block.uri_match] = block
+                else:
+                    block_overrides_by_uri_and_name[block.name] = block
+            elif block.name is not None:
+                block_overrides_by_name[block.name] = block
+            elif block_override_matching_all is None:
+                region_override_matching_all = block
+
+        self.region_overrides_by_name = region_overrides_by_name
+        self.region_overrides_by_uri = region_overrides_by_uri
+        self.region_overrides_by_uri_and_name = region_overrides_by_uri_and_name
+        self.region_override_matching_all = region_override_matching_all
+
+
     def render(self):
         '''Render the entire page, through a cascading series of calls to
         this class and render listeners.
         '''
-        
+
         # Make some fake data here, and pretend this came from plugins for now.
         fakedata = {
             'spam_block_one':{
                 'block_title':'Spam Block One',
                 'block_content':'Look, I\'m spam!',
                 'spam_index':0,
-            },
+                },
             'spam_block_two':{
                 'block_title':'Spam Block Two',
                 'block_content':'Look, I\'m spam!',
                 'spam_index':1,
-            },
+                },
         }
-        
+
         # The db knows which block goes in what region. Here we fake it
         block_locations_by_region = {
             'blog':(
                 'spam_block_one',
-            ),
+                ),
             'footer':(
                 'spam_block_two',
-            ),
+                ),
         }
-        
+
         # Cycle through each block_handler
-        
+
         # Store each region's rendered results here so it can then be fed to
         # the page template.
         rendered_regions = {}
-        
+
         # Store this locally for speed.
         blocks = self.block_handlers
-        
+
         # Loop through each item of the block locations dict, where the
         # Key is the region name, and the value is a list of blocks
         # belonging in that region.
         for region_name, block_names in block_locations_by_region.items():
             # For each rendered block, we append it to this string.
             rendered_region = ''
-            
+
             # Loop through each block_name.
             for block_name in block_names:
                 # Grab the block object, from self.blocks (locally assigned)
@@ -276,7 +326,7 @@ class Theme(object):
                 # function.
                 rendered_region += block_template.render(
                     **fakedata[block_name])
-        
+
         template = mako.template.Template(
             filename='%s/base.html' % self.theme_rel_path)
         return template.render(
@@ -291,46 +341,46 @@ class Theme(object):
         '''
         '''
         pass
-    
+
     def render_css_html(self):
         '''Render the html for the css files stored in this theme.
-        
+
         @returns: A string of the rendered css links, eg:
         	<link rel="stylesheet" 
                 href="/static/themes/core/usability/css/style.css"
                 type="text/css" media="screen, projection" />
-        
+
         '''
         # Store it locally for speed.
         theme_static_uri = self.theme_static_uri
-        
+
         def render_css(css_file):
             return css_file.render_html(base_theme_static_uri=theme_static_uri)
-        
+
         rendered_css_list = map(render_css, self.css_files)
         return ''.join(rendered_css_list)
-    
+
     def render_js_html(self):
         '''
         '''
         pass
-    
+
     def _get_rendered_css_html(self):
         '''Get self._rendered_css_html.
         '''
         if self._rendered_css_html is None:
             self._rendered_css_html = self.render_css_html()
         return self._rendered_css_html
-    
+
     rendered_css_html = property(_get_rendered_css_html)
-    
+
     def _get_rendered_js_html(self):
         '''Get self._rendered_js_html.
         '''
         if self._rendered_js_html is None:
             self._rendered_js_html = self.render_js_html()
         return self._rendered_js_html
-    
+
     rendered_js_html = property(_get_rendered_js_html)
 
 
@@ -352,7 +402,7 @@ class CoreTemplateOverride(object):
         This is basically only used by the theme developers, for plugin
         template overrides, see the rel_template_file param.
         @type local_template_file: A string path.
-        
+
         @param rel_template_file: The filename of the template file, relative
         to rocketseat's core dir.
         Eg:
@@ -360,24 +410,24 @@ class CoreTemplateOverride(object):
          - plugins/admin/theme/block-admin_login.html
         This is to be used by plugins mainly, as it allows them to give a path
         to any template file found in rocketseat.
-        
+
         @param template_text: If supplied, the handler will render from this
         and _not_ the template_file.
-        
+
         @param uri_match: If the uri does not match the regex supplied here,
         the block will not be shown.
         @type uri_match: A regex string or None.
         '''
-        
+
         if (local_template_file is None and rel_template_file is None
             and template_text is None):
             raise core.error.MissingTemplateInOverride()
-        
+
         self.local_template_file = local_template_file
         self.rel_template_file = rel_template_file
         self.template_text = template_text
         self.uri_match = uri_match
-    
+
     def get_template(self):
         '''Return the mako template object for this 
         '''
@@ -385,11 +435,11 @@ class CoreTemplateOverride(object):
         if self.template_text is not None:
             return mako.template.Template(
                 text=self.template_text)
-        
+
         # If we make it here, no template text was defined.. and there
         # _better_ be a template file... or all hell breaks loose.
         return mako.template.Template(
-                text=self.template_text)
+            text=self.template_text)
 
 class BaseTemplateOverride(CoreTemplateOverride):
     '''
@@ -415,24 +465,24 @@ class BlockTemplateOverride(CoreTemplateOverride):
         do so planning that it will be put into a block with a specific name.
         So as you might guess, naming this correctly is very important.
         @type name: A string or None.
-        
+
         @param inner_regions: If defined, the block will render the region(s)
         inside of it, along with any blocks the region(s) may contain
         @type inner_regions: A list or tuple or L{regions <Region>}.
-        
+
         @important: If both name and uri_match are None, all blocks that reach
         this instance will use it because the name matches (U{None is
         essentially a wildcard}), and the uri matches (U{Again, None is
         a wildcard}). This is useful if you want all blocks, no matter name or
         uri, to use a specific template.
-        
+
         Usually you put this "wildcard block" at the end of the blocks list, so
         that you can match any blocks you need, and if none of those match,
         your wildcard is chosen.
         '''
         super(BlockTemplateOverride, self).__init__(
             local_template_file, rel_template_file, template_text, uri_match)
-        
+
         self.name = name
         if inner_regions is not None:
             self.inner_regions = inner_regions
@@ -458,6 +508,6 @@ class RegionTemplateOverride(CoreTemplateOverride):
         '''
         super(RegionTemplateOverride, self).__init__(
             local_template_file, rel_template_file, template_text, uri_match)
-        
+
         self.name = name
-    
+
